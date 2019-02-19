7964ba51b6faa5bcbc8b3a9a2ee429f44021b686
jtzhang163 <jtzhang163@163.com>
2019-02-19 11:58:33 +0800

    显示注册页面和注册处理使用同一url

--- apps/user/views.py
+++ apps/user/views.py
@@ -8,44 +8,45 @@ import re
 
 
 def register(request):
-    return render(request, "register.html")
+
+    if request.method == 'GET':
+        # 显示注册页面
+        return render(request, "register.html")
+    else:
+        # 注册逻辑处理
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意协议'})
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            return render(request, 'register.html', {'errmsg': '用户已存在'})
+
+        # user = User()
+        # user.username = username
+        # user.password = password
+        # user.email = email
+        #
+        # user.save()
+
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        return redirect(reverse('goods:index'))
 
 
-# /user/register_handle
-def register_handle(request):
-    """进行注册处理"""
-
-    username = request.POST.get('user_name')
-    password = request.POST.get('pwd')
-    email = request.POST.get('email')
-    allow = request.POST.get('allow')
-
-    if not all([username, password, email]):
-        return render(request, 'register.html', {'errmsg': '数据不完整'})
-
-    if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-        return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
-
-    if allow != 'on':
-        return render(request, 'register.html', {'errmsg': '请同意协议'})
-
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        user = None
-
-    if user:
-        return render(request, 'register.html', {'errmsg': '用户已存在'})
-
-    # user = User()
-    # user.username = username
-    # user.password = password
-    # user.email = email
-    #
-    # user.save()
-
-    user = User.objects.create_user(username, email, password)
-    user.is_active = 0
-    user.save()
-
-    return redirect(reverse('goods:index'))
