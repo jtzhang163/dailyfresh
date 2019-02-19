c3d950938dcc4a389a7fe8a2da6c7cf0ba134939
jtzhang163 <jtzhang163@163.com>
2019-02-19 15:19:32 +0800

    使用类视图

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from user.views import RegisterView
 from user import views
 
 urlpatterns = [
-    url(r'^register$', views.register, name='register'),
+    # url(r'^register$', views.register, name='register'),
+    url(r'^register$', RegisterView.as_view(), name='register')
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse # 反向解析生成首页对应的地址
+from django.views.generic import View # 使用类视图
 from user.models import User
 import re
 
@@ -49,4 +50,47 @@ def register(request):
 
         return redirect(reverse('goods:index'))
 
+# /user/register
+class RegisterView(View):
+    """注册"""
+    def get(self, request):
+        """显示注册页面"""
+        return render(request, 'register.html')
+
+    def post(self, request):
+        """进行注册处理"""
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意协议'})
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            return render(request, 'register.html', {'errmsg': '用户已存在'})
+
+        # user = User()
+        # user.username = username
+        # user.password = password
+        # user.email = email
+        #
+        # user.save()
+
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        return redirect(reverse('goods:index'))
 
