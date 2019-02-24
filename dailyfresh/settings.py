63d622440a5046c6c495dd78cad3e05040744cbd
jtzhang163 <jtzhang163@163.com>
2019-02-24 11:51:40 +0800

    配置redis作为Django缓存和session存储后端

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -135,5 +135,21 @@ EMAIL_HOST_PASSWORD = 'zjt19930826'
 EMAIL_FROM = '天天生鲜<jtzhang163@163.com>'
 
 
+# Django的缓存配置
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/9",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# 配置session的存储
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
 
 
